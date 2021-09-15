00c1f4e0200dda795320c3bcc5cc0ea0e1e94687
Karoliina Salo <salo.karoliina@gmail.com>
2021-09-15 13:07:46 +0300

    Added content

--- tarinageneraattori/script.js
+++ tarinageneraattori/script.js
@@ -1,23 +1,32 @@
+// variables that hold all content for the page
 var genre = ["Fantasia", "Toiminta", "Romantiikka","Seikkailu", "Erotiikka", "Jännitys", "Kauhu", "Draama", "Mysteeri", "Komedia","Historia", "Sci-fi", "Rikos"];
 var fantasy = ["Nimi1", "Nimi2", "Nimi3"];
 var realF = ["Niina", "Elina", "Jenni", "Jenna"];
 var realM = ["M1", "M2", "M3"];
 var realO = ["Kuura"];
-
+// function that's linked to the button
 function generate() {
-
+  // list-variable that takes value from the dropdown-list
   var list = document.getElementById('dropdown');
+  // variable that takes value from list-variable
   var select = list.options[list.selectedIndex].value;
 
+  // if the user has selected 'genre' from the dropdown-list
+  // the program takes one random name from the 'genre' list
+  // and posts it on the output-area
   if (select == "genre")
   {
     var x = Math.floor(Math.random()*genre.length);
     document.getElementById('output').innerHTML = (genre[x]);
-
+    // variable 'previous' creates a new element 'list-item'
     var previous = document.createElement("li");
+    // variable 'text' takes the content of the 'output'
     var text = document.getElementById('output');
+    // the variable 'previous' is given the value of 'text'
     previous.innerHTML = text.innerHTML;
+    // variable 'previousElement' takes value from HTML-id 'previous'
     var previousElement = document.getElementById("previous");
+    // appendChild creates a new element from the 'previous'-variable and posts it on the DOM
     previousElement.appendChild(previous);
   }
   else if (select == "fantasy") {
