8cbd673d831ab8e93fd0ed21c337c8f9cb026573
Karoliina Salo <salo.karoliina@gmail.com>
2022-01-05 13:11:30 +0200

    Changed else-if statement to switch-case / 5.1.22 / K.

--- tarinageneraattori/script.js
+++ tarinageneraattori/script.js
@@ -1,73 +1,80 @@
 // variables that hold all content for the page
-var genre = ["Fantasia", "Toiminta", "Romantiikka","Seikkailu", "Erotiikka", "Jännitys", "Kauhu", "Draama", "Mysteeri", "Komedia","Historia", "Sci-fi", "Rikos"];
-var fantasy = ["Nimi1", "Nimi2", "Nimi3"];
-var realF = ["Niina", "Elina", "Jenni", "Jenna"];
-var realM = ["M1", "M2", "M3"];
-var realO = ["Kuura"];
+const genre = ["Fantasia", "Toiminta", "Romantiikka","Seikkailu", "Erotiikka", "Jännitys", "Kauhu", "Draama", "Mysteeri", "Komedia","Historia", "Sci-fi", "Rikos"];
+const fantasy = ["Nimi1", "Nimi2", "Nimi3"];
+const realF = ["Niina", "Elina", "Jenni", "Jenna"];
+const realM = ["M1", "M2", "M3"];
+const realO = ["Kuura", "Aalto"];
 // function that's linked to the button
 function generate() {
   // list-variable that takes value from the dropdown-list
-  var list = document.getElementById('dropdown');
+  let list = document.getElementById('dropdown');
   // variable that takes value from list-variable
-  var select = list.options[list.selectedIndex].value;
+  let select = list.options[list.selectedIndex].value;
 
-  // if the user has selected 'genre' from the dropdown-list
-  // the program takes one random name from the 'genre' list
-  // and posts it on the output-area
-  if (select == "genre")
-  {
-    var x = Math.floor(Math.random()*genre.length);
-    document.getElementById('output').innerHTML = (genre[x]);
-    // variable 'previous' creates a new element 'list-item'
-    var previous = document.createElement("li");
-    // variable 'text' takes the content of the 'output'
-    var text = document.getElementById('output');
-    // the variable 'previous' is given the value of 'text'
-    previous.innerHTML = text.innerHTML;
-    // variable 'previousElement' takes value from HTML-id 'previous'
-    var previousElement = document.getElementById("previous");
-    // appendChild creates a new element from the 'previous'-variable and posts it on the DOM
-    previousElement.appendChild(previous);
-  }
-  else if (select == "fantasy") {
-    x = Math.floor(Math.random()*fantasy.length);
-    document.getElementById('output').innerHTML = (fantasy[x]);
+// Switch case to check which option is selected
+// When option is selected, the program gives a random option from the selected variable
+// The random option is written in DOM
+  switch (select) {
+    case 'genre':
+      let a = Math.floor(Math.random()*genre.length);
+      document.getElementById('output').innerHTML = (genre[a]);
+      break;
+    case 'fantasy':
+      let b = Math.floor(Math.random()*fantasy.length);
+      document.getElementById('output').innerHTML = (fantasy[b]);
+      break;
+    case 'realF':
+      let c = Math.floor(Math.random()*realF.length);
+      document.getElementById('output').innerHTML = (realF[c]);
+      break;
+    case 'realM':
+      let d = Math.floor(Math.random()*realM.length);
+      document.getElementById('output').innerHTML = (realM[d]);
+      break;
+    default:
+      let e = Math.floor(Math.random()*realO.length);
+      document.getElementById('output').innerHTML = (realO[e]);
+      break;
+    }
 
-    var previousSecond = document.createElement("li");
-    text = document.getElementById('output');
-    previousSecond.innerHTML = text.innerHTML;
-    previousElement = document.getElementById("previousSecond");
-    previousElement.appendChild(previousSecond);
-  }
-  else if (select == "realF") {
-    x = Math.floor(Math.random()*realF.length);
-    document.getElementById('output').innerHTML = (realF[x]);
-
-    var previousThird = document.createElement("li");
-    text = document.getElementById('output');
-    previousThird.innerHTML = text.innerHTML;
-    previousElement = document.getElementById("previousThird");
-    previousElement.appendChild(previousThird);
-  }
-  else if (select == "realM") {
-    x = Math.floor(Math.random()*realM.length);
-    document.getElementById('output').innerHTML = (realM[x]);
-
-    var previousFourth = document.createElement("li");
-    text = document.getElementById('output');
-    previousFourth.innerHTML = text.innerHTML;
-    previousElement = document.getElementById("previousFourth");
-    previousElement.appendChild(previousFourth);
-  }
-  else {
-    x = Math.floor(Math.random()*realO.length);
-    document.getElementById('output').innerHTML = (realO[x]);
-
-    var previousFifth = document.createElement("li");
-    text = document.getElementById('output');
-    previousFifth.innerHTML = text.innerHTML;
-    previousElement = document.getElementById("previousFifth");
-    previousElement.appendChild(previousFifth);
-
+    // Two separate switch-cases just to keep the code clean and simple for me to understand
+    // This is a continuation to the previous switch-case
+    // This switch case checks the selected option and writes it into a DOM as a list-element
+      switch (select) {
+        case 'genre':
+          let previousFirst = document.createElement("li");
+          text = document.getElementById('output');
+          previousFirst.innerHTML = text.innerHTML;
+          previousElement = document.getElementById("previousFirst");
+          previousElement.appendChild(previousFirst);
+          break;
+        case 'fantasy':
+          let previousSecond = document.createElement("li");
+          text = document.getElementById('output');
+          previousSecond.innerHTML = text.innerHTML;
+          previousElement = document.getElementById("previousSecond");
+          previousElement.appendChild(previousSecond);
+          break;
+        case 'realF':
+          let previousThird = document.createElement("li");
+          text = document.getElementById('output');
+          previousThird.innerHTML = text.innerHTML;
+          previousElement = document.getElementById("previousThird");
+          previousElement.appendChild(previousThird);
+          break;
+        case 'realM':
+          let previousFourth = document.createElement("li");
+          text = document.getElementById('output');
+          previousFourth.innerHTML = text.innerHTML;
+          previousElement = document.getElementById("previousFourth");
+          previousElement.appendChild(previousFourth);
+          break;
+        default:
+          let previousFifth = document.createElement("li");
+          text = document.getElementById('output');
+          previousFifth.innerHTML = text.innerHTML;
+          previousElement = document.getElementById("previousFifth");
+          previousElement.appendChild(previousFifth);
+          break;
   }
 }
