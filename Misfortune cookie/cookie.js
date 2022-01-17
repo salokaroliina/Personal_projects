ebd5e02cc28257c71093182efbb9457c0fd540ba
Karoliina Salo <salo.karoliina@gmail.com>
2022-01-17 19:38:15 +0200

    Fixed var to let / 17.1.22 / K.

--- Misfortune cookie/cookie.js
+++ Misfortune cookie/cookie.js
@@ -1,11 +1,10 @@
-function fortuneCookie()
-{
+function fortuneCookie(){
   // variable that gives random sentences
-  var misfortune = ["Better stay home tomorrow", "Did you really turn off the stove?", "Your neighbor knows what you did",
+  let misfortune = ["Better stay home tomorrow", "Did you really turn off the stove?", "Your neighbor knows what you did",
   "There was a spider under your pillow", "You better check your bank account soon", "Do they really care about you?",
   "They talk about you behind your back and you know it"];
     // variable that takes random sentence from var 'misfortune'
-    var x = Math.floor(Math.random() * misfortune.length);
+    let x = Math.floor(Math.random() * misfortune.length);
     // log shows which misfortune the player got
     console.log(misfortune[x]);
     // takes the id 'fortune' from HTML and posts misfortune[x] on its place
